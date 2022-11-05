6e60f3a2bfdb44327526cc3ed81dae4ad786f56a
igwemiracle <igwemiracle35@gmail.com>
2022-11-05 09:07:43 +0100

    first commit

---
+++ enums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Operations(Enum):
+    ADDITION = 'addition'
+    SUBTRACTION = 'subtraction'
+    MULTIPLICATION = 'multiplication'
+
+
--- operations.py
+++ operations.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+from enums import Operations
+
 
 app = Flask(__name__)
 
@@ -7,13 +9,13 @@ def operation():
   operation_type = request.json['operation_type']
   x = request.json['x']
   y = request.json['y']
-  if(operation_type == 'addition'):
-    result = x + y
-  elif(operation_type == 'subtraction'):
-    result = x - y
-  elif(operation_type == 'multiplication'):
-    result = x * y
-  payload = {'slackUserName': 'Miracle','operation_type': operation_type, 'result':result }
+  if(operation_type == Operations.ADDITION.value):
+    operation_result = x + y
+  elif(operation_type == Operations.SUBTRACTION.value):
+    operation_result = x - y
+  elif(operation_type == Operations.MULTIPLICATION.value):
+    operation_result = x * y
+  payload = {'slackUserName': 'Miracle','operation_type': operation_type, 'result':operation_result }
   return jsonify(payload)
 
 
